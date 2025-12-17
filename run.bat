8c11e8faddefb9f89509369c35c2bcaf2dfa4a56
Igor Marlon <igormarlon.o@gmail.com>
2025-12-17 00:16:13 -0300

    chore: remove batch files from git tracking

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if not exist "bin" mkdir bin
-echo Compilando...
-javac -d bin -sourcepath src src/sistemabancario/*.java
-if %errorlevel% neq 0 exit /b %errorlevel%
-echo Executando...
-java -cp bin sistemabancario.Programa
